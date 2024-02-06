b098c1aafc3b4851d04b18824c63f56d7a32513c
Regina <ashreggie.luv@gmail.com>
2024-02-06 19:08:34 +0800

    add app.py and data file

---
+++ data/app.py
@@ -0,0 +1,15 @@
+import sqlite3
+import pandas as pd
+try:
+    con = sqlite3.connect('./data/score.db')
+except Exception as err:
+    print(f"Connection error:\n{err}")
+
+# Extract data as pd.DataFrame
+cursor = con.cursor()
+raw_df = pd.read_sql_query('SELECT * FROM score', con)
+
+# Close db connection
+con.close()
+
+print(raw_df)
